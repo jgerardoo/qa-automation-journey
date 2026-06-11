42e174210e2f92e594c13a75f21c59d3b352a8f4
Jesse Varela <jesse.varela90@gmail.com>
2026-06-11 11:44:39 -0500

    Adding examples and practice files for loops

---
+++ javascript/06_loops/loops_practice.js
@@ -0,0 +1,10 @@
+/*
+=======================================================
+Learn JavaScript - Codecademy | Lesson 6: Loops
+Practice exercise from the course lesson concepts.
+Project practice: Whale talk
+    Concepts: Loops in JavaScript
+=======================================================
+
+Practice loops in JavaScript by building a project that will translate English into “Whale Talk".
+*/
