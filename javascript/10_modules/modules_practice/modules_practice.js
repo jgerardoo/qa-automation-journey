3f6150dc1b97f94ea9d3fd3741ebf436d47929da
Jesse Varela <jesse.varela90@gmail.com>
2026-07-22 12:44:51 -0500

    Adding modules practice file (TBC)

---
+++ javascript/10_modules/modules_practice/modules_practice.js
@@ -0,0 +1,11 @@
+/*
+====================================================================
+Learn Intermediate JavaScript - Codecademy | Lesson 2: Modules
+Practice exercise from the course lesson concepts.
+Project practice 1: Message Mixer
+    Concepts: Modules in JavaScript (Implementing CommonJS Modules)
+====================================================================
+
+Message Mixer Inc. offers a message-encryption service that transforms input text, using various
+ciphers, and displays the encrypted message to the console.
+*/
